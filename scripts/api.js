4ec9123a37a9dd72fb6cbf96f118b61723e2f2a5
Tammy Tian <tiantammy@outlook.com>
2018-04-12 00:26:34 +0800

    fixed api.js

--- scripts/api.js
+++ scripts/api.js
@@ -3,7 +3,7 @@
 const api = (function () {
   let BASE_URL = 'https://thinkful-list-api.herokuapp.com/tammy-dameon';
   
-  let getItems = function(callback) {
+  let getItems = function(name, callback) {
     $.getJSON(`${BASE_URL}/items`, callback);
     callback();
   };
